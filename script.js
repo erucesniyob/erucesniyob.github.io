51dd4b265781a5ecaceaf8f4a24dd7bad9115e89
erucesniyob <44059918+erucesniyob@users.noreply.github.com>
2020-03-01 19:58:16 +0700

    4th

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ fetch( "https://hacker-news.firebaseio.com/v0/topstories.json")
 .then(response => response.json())
 .then(data =>{
     topnewz = data;
-});
+})
+.catch(error =>{
+    console.log(error);
+})
 
 
 
